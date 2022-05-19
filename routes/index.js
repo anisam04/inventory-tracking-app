b9d093238047574f07940bfb939877336cd02bfb
ani0104 <annie.samajpati@gmail.com>
2022-05-19 17:20:48 -0400

    1.bootstrap nav 2. index(c, r and v) 3.new gitignore

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,5 @@ var express = require('express');
 var router = express.Router();
 const indexCtrl = require('../controllers/indexCtrl')
 
-router.get('/readme', indexCtrl.getreadmePage);
+router.get('/', indexCtrl.getreadmePage);
 module.exports = router;
